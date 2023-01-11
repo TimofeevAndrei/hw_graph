cd7c9f8d5b938c22d8211ce7a855f295d8f896c0
TimofeevAndrei <i.pika4u@gmail.com>
2023-01-11 10:29:54 +0700

    done

--- script.sql
+++ script.sql
@@ -1,45 +1,45 @@
 CREATE TABLE IF NOT EXISTS genre (
-	genre_id INTEGER PRIMARY KEY,
-	name VARCHAR(80) UNIQUE NOT NULL,
+	genre_id INTEGER PRIMARY key,
+	name VARCHAR(80) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS artist (
-	artist_id INTEGER PRIMARY KEY,
-	genre VARCHAR(80) NOT NULL,
+	artist_id INTEGER PRIMARY key,
+	genre VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS album (
-	album_id INTEGER PRIMARY KEY,
-	name VARCHAR(80) NOT NULL,
-	relese_date VARCHAR(80) NOT NULL,
+	album_id INTEGER PRIMARY key,
+	name VARCHAR(80) NOT null,
+	relese_date VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS song (
-	song_id INTEGER PRIMARY KEY,
-	duration VARCHAR(80) NOT NULL,
-	album VARCHAR(80) FOREIGN KEY REFERENCES album(album_id) NOT NULL,
+	song_id INTEGER PRIMARY key,
+	duration VARCHAR(80) NOT null,
+	album INTEGER REFERENCES album(album_id) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS collection (
-	collection_id INTEGER PRIMARY KEY,
-	date VARCHAR(80) NOT NULL,
-	name VARCHAR(80) NOT NULL,
+	collection_id INTEGER PRIMARY key,
+	date VARCHAR(80) NOT null,
+	name VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS collection_list (
-	song_id INTEGER FOREIGN KEY REFERENCES song(song_id),
-	collection_id INTEGER FOREIGN KEY REFERENCES collection(collection_id),
-	constraint id PRIMARY KEY (song_id, collection_id)
+	song_id INTEGER REFERENCES song(song_id),
+	collection_id INTEGER REFERENCES collection(collection_id),
+	constraint id_coll PRIMARY KEY (song_id, collection_id)
 );
 
 CREATE TABLE IF NOT EXISTS author_list (
-	artist_id INTEGER FOREIGN KEY REFERENCES artist(artist_id),
-	album_id INTEGER FOREIGN KEY REFERENCES album(album_id),
-	constraint id PRIMARY KEY (artist_id, album_id)
+	artist_id INTEGER REFERENCES artist(artist_id),
+	album_id INTEGER REFERENCES album(album_id),
+	constraint id_author PRIMARY KEY (artist_id, album_id)
 );
 
 CREATE TABLE IF NOT EXISTS genre_list (
-	artist_id INTEGER FOREIGN KEY REFERENCES artist(artist_id),
-	genre_id INTEGER FOREIGN KEY REFERENCES genre(genre_id),
-	constraint id PRIMARY KEY (artist_id, genre_id)
+	artist_id INTEGER REFERENCES artist(artist_id),
+	genre_id INTEGER REFERENCES genre(genre_id),
+	constraint id_genlist PRIMARY KEY (artist_id, genre_id)
 );
